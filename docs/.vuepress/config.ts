22ff31c697d4d3e230b7565344a8917e72ba9c05
linwu-hi <linwu.hi@gmail.com>
2023-07-01 12:16:22 +0800

    feat: update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -9,6 +9,8 @@ export default defineUserConfig({
   base,
   dest: "./dist",
   head:[
+    ['meta', { name: 'baidu-site-verification', content: 'codeva-IkQhVRqkxI' }],
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
     ['link', { rel: 'icon', href: '/assets/image/icon.png' }]
   ],
   locales: {
